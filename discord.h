4dceb2af66900f9d55c5cc4c0d1ed2597fb10605
f0xy <alex.foxy.rain@gmail.com>
2020-07-27 13:14:56 +0000

    fix + improve

--- discord.h
+++ discord.h
@@ -1,3 +1,5 @@
+#ifndef DISCORD_H
+#define DISCORD_H
 #include <curl/curl.h>
 #include <iostream>
 #include <algorithm>
@@ -7,4 +9,5 @@
 
 int discord_request(std::string json_data);
 int discord_game_created(std::string game_name, std::string game_owner, std::string map_path);
-int discord_bug_message(std::string game_name, std::string map_path, std::string bug_provider, std::string bug_description)
+int discord_bug_message(std::string game_name, std::string map_path, std::string bug_provider, std::string bug_description);
+#endif
