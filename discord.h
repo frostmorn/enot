4788ea26c3894c730fc47d8e94a784c96fab6088
f0xy <alex.foxy.rain@gmail.com>
2020-07-27 13:11:36 +0000

    fix compile

--- discord.h
+++ discord.h
@@ -9,3 +9,4 @@
 std::string escape_json(const std::string &s);
 int discord_request(std::string json_data);
 int discord_game_created(std::string game_name, std::string game_owner, std::string map_path);
+int discord_bug_message(std::string game_name, std::string map_path, std::string bug_provider, std::string bug_description)
