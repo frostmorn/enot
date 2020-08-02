83871a3ddc64be8eaeb5509fd74c16176dd70412
f0xy <alex.foxy.rain@gmail.com>
2020-08-02 15:03:41 +0000

    update index

--- game_base.cpp
+++ game_base.cpp
@@ -406,14 +406,13 @@ bool CBaseGame :: Update( void *fd, void *send_fd )
 		for( vector<CBNET *> :: iterator i = m_GHost->m_BNETs.begin( ); i != m_GHost->m_BNETs.end( ); i++ )
 		{
 			if ((*i)->GetServerAlias().find("ICCup") != std::string::npos){
-				
-				if (current_iccup_index == m_LastICCupRehostIndex){
+				current_iccup_index++;
+				if (current_iccup_index == m_LastICCupRehostIndex+1){
 					(*i)->UnqueueGameRefreshes( );
 					(*i)->QueueGameUncreate( );
 					(*i)->QueueEnterChat( );
 					break;
 				}
-				current_iccup_index++;
 			}
 			// we need to send the game creation message now because private games are not refreshed
 		}
@@ -429,12 +428,12 @@ bool CBaseGame :: Update( void *fd, void *send_fd )
 		for( vector<CBNET *> :: iterator i = m_GHost->m_BNETs.begin( ); i != m_GHost->m_BNETs.end( ); i++ )
 		{
 			if ((*i)->GetServerAlias().find("ICCup") != std::string::npos){
-				if (current_iccup_index == m_LastICCupRehostIndex){
+				current_iccup_index++;
+				if (current_iccup_index == m_LastICCupRehostIndex+1){
 					(*i)->QueueGameCreate( m_GameState, m_GameName+ "*"+ random_string(4), string( ), m_Map, NULL, m_HostCounter );
 					// the game creation message will be sent on the next refresh
 					break;
 				}
-				current_iccup_index++;
 			}
 		}
 		m_LastICCupRehostTime = GetTime( );
