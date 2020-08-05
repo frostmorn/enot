58b43ac48f13325b7b269991089f9d415bbfb35c
f0xy <alex.foxy.rain@gmail.com>
2020-08-05 20:23:38 +0000

    random_string fix

--- util.cpp
+++ util.cpp
@@ -594,7 +594,21 @@ bool UTIL_IsLanIP( BYTEARRAY ip )
 
 	return false;
 }
-
+std::string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
 bool UTIL_IsLocalIP( BYTEARRAY ip, vector<BYTEARRAY> &localIPs )
 {
 	if( ip.size( ) != 4 )
