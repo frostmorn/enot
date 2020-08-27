8d2c1ce7f70420a64c4edc8a452ed8e173f64614
f0xy <alex.foxy.rain@gmail.com>
2020-08-27 03:12:49 +0000

    Update

--- bnet.cpp
+++ bnet.cpp
@@ -783,7 +783,7 @@ void CBNET :: ProcessPackets( )
 					}
 					
 					if (m_GHost->m_CurrentGame)
-						CONSOLE_Print("Online Players > Lobby:"+UTIL_ToString(m_GHost->m_CurrentGame->GetNumPlayers()) +" In games:"+ UTIL_ToString(total_players_count));
+						CONSOLE_Print("Online Players > L:"+UTIL_ToString(m_GHost->m_CurrentGame->GetNumPlayers()) +" Total:"+ UTIL_ToString(total_players_count));
 				}
 				else
 				{
