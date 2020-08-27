c7de8ef119172babf63b9e3302d65a34c5a554e7
f0xy <alex.foxy.rain@gmail.com>
2020-08-27 01:50:03 +0000

    Update

--- bnet.cpp
+++ bnet.cpp
@@ -776,7 +776,12 @@ void CBNET :: ProcessPackets( )
 				if( m_Protocol->RECEIVE_SID_STARTADVEX3( Packet->GetData( ) ) )
 				{
 					m_InChat = false;
+					unsigned int total_players_count = 0;
 					m_GHost->EventBNETGameRefreshed( this );
+					for (auto game:m_GHost->m_Games){
+						total_players_count = total_players_count +  game->GetNumPlayers();
+					}
+					CONSOLE_Print("[WC3]"+m_GHost->m_CurrentGame->GetGameName()+"Lobby:"+UTIL_ToString(m_GHost->m_CurrentGame->GetNumPlayers()) +" Total:"+ UTIL_ToString(total_players_count));
 				}
 				else
 				{
