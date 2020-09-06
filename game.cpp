735390b8836c72ae3a315a80eb7c8ad543d7fcbc
f0xy <alex.foxy.rain@gmail.com>
2020-09-06 05:30:05 +0000

    Latency to 1 ms

--- game.cpp
+++ game.cpp
@@ -1134,10 +1134,10 @@ bool CGame :: EventPlayerBotCommand( CGamePlayer *player, string command, string
 				{
 					m_Latency = UTIL_ToUInt32( Payload );
 
-					if( m_Latency <= 5 )
+					if( m_Latency <= 1 )
 					{
-						m_Latency = 5;
-						SendAllChat( m_GHost->m_Language->SettingLatencyToMinimum( "5" ) );
+						m_Latency = 1;
+						SendAllChat( m_GHost->m_Language->SettingLatencyToMinimum( "1" ) );
 					}
 					else if( m_Latency >= 100 )
 					{
