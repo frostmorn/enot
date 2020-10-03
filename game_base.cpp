cf09191b806bb9097938c263669f1dfa139c1b0e
f0xy <alex.foxy.rain@gmail.com>
2020-10-03 05:31:17 +0000

    fix

--- game_base.cpp
+++ game_base.cpp
@@ -684,7 +684,7 @@ bool CBaseGame :: Update( void *fd, void *send_fd )
 
 			if( (*i)->GetOutPacketsQueued( ) <= 1 )
 			{
-				if (((*i)->GetServerAlias().find("ICCup") == std::string::npos)&&((*i)->GetServerAlias().find("ICCup") == std::string::npos)){
+				if (((*i)->GetServerAlias().find("ICCup") == std::string::npos)&&((*i)->GetServerAlias().find("Rubattle") == std::string::npos)){
 					(*i)->QueueGameRefresh( m_GameState, m_GameName, string( ), m_Map, m_SaveGame, 0, m_HostCounter );
 				}
 				Refreshed = true;
