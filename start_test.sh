f4f951c0d9fb63c13025727817eec8a8dee12aec
Orphey <15167344+frostmorn@users.noreply.github.com>
2021-11-26 13:19:34 +0000

    update .gitignore, del production files

--- start_test.sh
+++
@@ -1,6 +0,0 @@
-export LC_ALL=ru_RU.UTF-8
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:CascLib/build/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:bncsutil/src/bncsutil/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:StormLib/stormlib/
-
-./ghost++ test.cfg
